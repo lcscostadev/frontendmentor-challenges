7e8fbf496b8da33d013f96edd1f196aef19f66b6
Lucas Costa <thelucascostadasilva@gmail.com>
2023-02-13 21:29:28 -0300

    add new md, lg responsiveness

--- Junior/blogr-landing-page-main/blogr-landing-page/src/App.jsx
+++ Junior/blogr-landing-page-main/blogr-landing-page/src/App.jsx
@@ -6,11 +6,11 @@ import { patternDestktop, patternMobile } from './assets';
 function App() {
   return (
     <>
-      <div className='max-w-[425px] xl:max-w-[1440px]'>
+      <div className='max-w-[425px] md:max-w-[768px] lg:max-w-[1024px] xl:max-w-[100%]'>
         <header className='bg-gradient-to-t from-[#ff525d] to-[#FF846D] rounded-bl-[100px] h-[65vh] relative overflow-hidden'>
-          <img src={patternMobile} alt="" className='absolute top-[-40%] left-[-60%] max-w-[1200px]' />
+          <img src={patternMobile} alt="pattern" className='absolute top-[-40%] left-[-60%] max-w-[1200px]' />
           <Navbar />
-          <img src={patternDestktop} alt="" className='absolute top-[-200%] left-[-10%] max-w-[2800px]' />
+          <img src={patternDestktop} alt="pattern" className='absolute top-[-200%] left-[-10%] max-w-[2800px]' />
           <Hero />
         </header>
         <main>
--- Junior/blogr-landing-page-main/blogr-landing-page/src/assets/index.js
+++ Junior/blogr-landing-page-main/blogr-landing-page/src/assets/index.js
@@ -13,5 +13,3 @@ export {
     ilustrationMobile,
     ilustrationDesktop,
 }
-
-// test
